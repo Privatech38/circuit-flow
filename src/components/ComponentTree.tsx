a94f2e90b9ae1d2e5702e5b722ba7e16a15f0360
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-05-26 19:55:45 +0200

    refactor(UI): remove tree view dependency and replace with custom one

--- src/components/ComponentTree.tsx
+++ src/components/ComponentTree.tsx
@@ -1,17 +1,49 @@
-import {SimpleTreeView} from '@mui/x-tree-view/SimpleTreeView';
-import {TreeItem} from '@mui/x-tree-view/TreeItem';
+import type {ComponentType, SVGProps, ReactElement} from 'react';
 import AndGateSvg from '../assets/components/gates/AndGate.svg?react';
+import OrGateSvg from '../assets/components/gates/OrGate.svg?react';
+import BufferSVG from '../assets/components/gates/BufferGate.svg?react';
+import './ComponentTree.css';
 
 export default function ComponentTree() {
     return (
         <div style={{textAlign: 'left'}}>
-            <SimpleTreeView>
-                <TreeItem itemId={"logicGates"} label="Logic Gates">
-                    <TreeItem itemId={"andGate"} label="AND Gate"
-                              slots={{icon: () => <AndGateSvg stroke-width={6} viewBox="-3 -3 61 56"/>}}
-                    />
-                </TreeItem>
-            </SimpleTreeView>
+            <ComponentCategory label={"Logic Gates"}>
+                <ComponentItem label={"AND Gate"} icon={AndGateSvg} />
+                <ComponentItem label={"OR Gate"} icon={OrGateSvg} />
+                <ComponentItem label={"Buffer"} icon={BufferSVG} />
+            </ComponentCategory>
         </div>
     )
+
+    function ComponentCategory({ children, label }: ComponentCategoryProps) {
+        return (
+            <div className="component-category" aria-label={label}>
+                <h4 className="component-category__label">{label}</h4>
+                <div className="component-category__content">
+                    {children}
+                </div>
+            </div>
+        )
+    }
+
+    function ComponentItem({ label, icon: Icon }: ComponentItemProps) {
+        return (
+            <div className="component-item" aria-label={label}>
+                <div className="component-item__icon" aria-hidden="true">
+                    <Icon />
+                </div>
+                <div className="component-item__label">{label}</div>
+            </div>
+        )
+    }
+}
+
+interface ComponentCategoryProps {
+    children?: ReactElement<ComponentItemProps> | ReactElement<ComponentItemProps>[];
+    label: string;
+}
+
+interface ComponentItemProps {
+    label: string;
+    icon: ComponentType<SVGProps<SVGSVGElement>>
 }
