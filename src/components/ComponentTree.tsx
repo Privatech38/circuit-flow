884a7596e162fb789e61d4497df335c816edc7ac
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-05-26 13:19:02 +0200

    feat(ComponentTree): integrate AndGate SVG and add vite-plugin-svgr support

--- src/components/ComponentTree.tsx
+++ src/components/ComponentTree.tsx
@@ -1,15 +1,17 @@
-import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
-
+import {SimpleTreeView} from '@mui/x-tree-view/SimpleTreeView';
+import {TreeItem} from '@mui/x-tree-view/TreeItem';
+import AndGateSvg from '../assets/components/gates/AndGate.svg?react';
 
 export default function ComponentTree() {
     return (
-        <div style={{textAlign:'left'}}>
+        <div style={{textAlign: 'left'}}>
             <SimpleTreeView>
                 <TreeItem itemId={"logicGates"} label="Logic Gates">
-                    <TreeItem itemId={"andGate"} label="AND Gate" />
+                    <TreeItem itemId={"andGate"} label="AND Gate"
+                              slots={{icon: () => <AndGateSvg stroke-width={6} viewBox="-3 -3 61 56"/>}}
+                    />
                 </TreeItem>
             </SimpleTreeView>
         </div>
-        )
+    )
 }
