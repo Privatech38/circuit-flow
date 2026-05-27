2fe8c4ce081e6764191904511214e79fe7009be2
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-05-27 10:58:11 +0200

    feat(UI): Add all logic gates to the category

--- src/components/ComponentTree.tsx
+++ src/components/ComponentTree.tsx
@@ -2,15 +2,25 @@ import type {ComponentType, SVGProps, ReactElement} from 'react';
 import AndGateSvg from '../assets/components/gates/AndGate.svg?react';
 import OrGateSvg from '../assets/components/gates/OrGate.svg?react';
 import BufferSVG from '../assets/components/gates/BufferGate.svg?react';
+import NOTGateSVG from '../assets/components/gates/NotGate.svg?react';
+import NANDGateSVG from '../assets/components/gates/NAndGate.svg?react';
+import NORGateSVG from '../assets/components/gates/NOrGate.svg?react';
+import XORGateSVG from '../assets/components/gates/XOrGate.svg?react';
+import XNORGateSVG from '../assets/components/gates/XNOrGate.svg?react';
 import './ComponentTree.css';
 
 export default function ComponentTree() {
     return (
         <div style={{textAlign: 'left'}}>
             <ComponentCategory label={"Logic Gates"}>
-                <ComponentItem label={"AND Gate"} icon={AndGateSvg} />
-                <ComponentItem label={"OR Gate"} icon={OrGateSvg} />
+                <ComponentItem label={"AND"} icon={AndGateSvg} />
+                <ComponentItem label={"OR"} icon={OrGateSvg} />
+                <ComponentItem label={"NOT"} icon={NOTGateSVG} />
                 <ComponentItem label={"Buffer"} icon={BufferSVG} />
+                <ComponentItem label={"NAND"} icon={NANDGateSVG} />
+                <ComponentItem label={"NOR"} icon={NORGateSVG} />
+                <ComponentItem label={"XOR"} icon={XORGateSVG} />
+                <ComponentItem label={"XNOR"} icon={XNORGateSVG} />
             </ComponentCategory>
         </div>
     )
