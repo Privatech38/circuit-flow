9e6f74e25becb62be0ae1752f066baad43e90f25
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-06-15 15:01:44 +0200

    fix(UI): Alternative text for SVGs being "Buffer Gate"

--- src/components/input/Clock.tsx
+++ src/components/input/Clock.tsx
@@ -7,7 +7,7 @@ import ClockSVG from '@assets/components/input/Clock.svg';
 export default function Clock() {
     return (
         <div style={{position: 'relative', lineHeight: 0}}>
-            <img src={ClockSVG} alt={"Buffer Gate"} height={50} />
+            <img src={ClockSVG} alt={"Clock"} height={50} />
 
             {/* Output handle */}
             <Handle type="source" position={Position.Right} id="out"/>
--- src/components/input/High.tsx
+++ src/components/input/High.tsx
@@ -7,7 +7,7 @@ import NodeSVG from '@assets/components/input/High.svg';
 export default function High() {
     return (
         <div style={{position: 'relative', lineHeight: 0}}>
-            <img src={NodeSVG} alt={"Buffer Gate"} height={50} />
+            <img src={NodeSVG} alt={"High Signal"} height={50} />
 
             {/* Output handle */}
             <Handle type="source" position={Position.Right} id="out"/>
