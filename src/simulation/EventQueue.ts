2dccb42d6b49fa30b4b2419745b28f261086c6c0
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-07-01 14:41:41 +0200

    fix: triggerUpdate now uses a type to fix TS error

--- src/simulation/EventQueue.ts
+++ src/simulation/EventQueue.ts
@@ -1,5 +1,7 @@
 import type {Node} from "@xyflow/react";
 import {logicGates} from "@/components/gates";
+import {outputComponents} from "@/components/output";
+import {inputComponents} from "@/components/input";
 
 /**
  * A map containing node ID - interval ID pairs.
@@ -7,11 +9,22 @@ import {logicGates} from "@/components/gates";
 const intervals: Map<string, number> = new Map();
 
 const nodeEvaluators = {
-    ...logicGates
+    ...logicGates,
+    ...inputComponents,
+    ...outputComponents,
 }
 
+type NodeType = keyof typeof nodeEvaluators;
+
+/**
+ * Triggers an update on the specified node.
+ * @param node
+ */
 export function triggerUpdate(node: Node) {
     if (!node.type)
         throw new Error("Type for node is undefined");
-    nodeEvaluators[node.type].evaluate(node);
+    if (node.type in nodeEvaluators) {
+        const evaluator = nodeEvaluators[node.type as NodeType];
+        evaluator.evaluate(node);
+    }
 }
