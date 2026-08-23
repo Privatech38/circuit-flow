bdd65a8644ba57a2cb0277aaf8b903b4fdc01942
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-08-23 21:06:31 +0200

    feat: Enhance PoweredEdge component with improved edge path calculation and styling updates

--- src/editor/PoweredEdge.tsx
+++ src/editor/PoweredEdge.tsx
@@ -1,16 +1,23 @@
-import { getSmoothStepPath, BaseEdge, type EdgeProps, type Edge } from '@xyflow/react';
-import { useEffect } from 'react';
+import {BaseEdge, type Edge, type EdgeProps, getSmoothStepPath, Position} from '@xyflow/react';
+import {useEffect} from 'react';
 import {getNodeOutputState, updateEdgeStyle} from "@/simulation/WireManager.ts";
 
 type PoweredEdge = Edge<Record<string, never>>
 
 export function PoweredEdge({id, sourceX, sourceY, targetX, targetY, source, sourceHandleId}: EdgeProps<PoweredEdge>) {
-    const [edgePath] = getSmoothStepPath({ sourceX, sourceY, targetX, targetY });
+    const [edgePath] = getSmoothStepPath({
+        sourceX: sourceX,
+        sourceY: sourceY,
+        targetX: targetX,
+        targetY: targetY,
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left
+    });
 
     useEffect(() => {
-        const nodeOutputState = getNodeOutputState({ id: source });
-        updateEdgeStyle({ id: id }, sourceHandleId ? nodeOutputState.has(sourceHandleId) : nodeOutputState.size > 0)
+        const nodeOutputState = getNodeOutputState({id: source});
+        updateEdgeStyle({id: id}, sourceHandleId ? nodeOutputState.has(sourceHandleId) : nodeOutputState.size > 0)
     }, [id, source, sourceHandleId]);
 
-    return <BaseEdge id={id} path={edgePath} />;
+    return <BaseEdge id={id} path={edgePath}/>;
 }
