15a693c25d5544a161d4f23aaad0b6946030c534
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-08-24 08:34:31 +0200

    fix: update dirname path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets')
+      '@': path.resolve(import.meta.dirname, 'src'),
+      '@assets': path.resolve(import.meta.dirname, 'src/assets')
     }
   }
 })
