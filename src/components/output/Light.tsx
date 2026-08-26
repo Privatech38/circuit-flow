f80d10788f054a49d80fc2e0903a6f27fcb1c78f
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-08-26 21:05:46 +0200

    feat(Light): Enhance light component with CSS transitions and class-based state management

--- src/components/output/Light.tsx
+++ src/components/output/Light.tsx
@@ -3,28 +3,20 @@ import {
     Position,
     type Node
 } from '@xyflow/react';
-import LightSVG from '@assets/components/output/Light.svg';
+import LightSVG from '@assets/components/output/Light.svg?react';
 import type {CircuitComponent} from "@/components/Component.ts";
 import {getHandleState} from "@/simulation/ReactFlowUtils.ts";
 
 export const Light: CircuitComponent = {
     evaluate: (node: Node) => {
-        if (getHandleState(node, { id: "in" })) {
-            node.style = {
-                ...node.style,
-                fill: '#f00'
-            };
-        } else {
-            node.style = {
-                ...node.style,
-                fill: '#fff'
-            };
-        }
+        const isOn = getHandleState(node);
+        const bulb = document.querySelector(`.react-flow__node[data-id="${node.id}"] .light-bulb`);
+        bulb?.classList.toggle('signal-high', isOn);
     },
 
     component: () => (
         <div style={{position: 'relative', lineHeight: 0}}>
-            <img src={LightSVG} alt={"Light"} height={50} />
+            <LightSVG className="light-bulb" height={50}/>
 
             {/* Input handle */}
             <Handle type="target" position={Position.Left} id="in"/>
